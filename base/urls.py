33faa286b832c9c5f5db820c9bb1af98eae8019c
Diego <ddglobaltravel1@gmail.com>
2022-05-18 22:18:40 -0500

    Arreglos en el SEO

--- base/urls.py
+++ base/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 from . import views
 from django.contrib.sitemaps.views import sitemap
 from .sitemaps import StaticSitemap, CategorySitemap
+from django.views.generic.base import TemplateView
 
 sitemaps = {
     'static': StaticSitemap,
     'categories': CategorySitemap
 }
 
-
 urlpatterns = [
     path('', views.home, name="home"),
     path('test', views.test, name="test"),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-        name='django.contrib.sitemaps.views.sitemap')
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"))
 ]
